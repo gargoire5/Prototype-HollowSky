8a38065e084a5963592c88ac9240d29be2c09777
skybloc4 <117281120+skybloc4@users.noreply.github.com>
2025-06-06 14:01:11 +0200

    dash end and ennemy detect zone

--- HollowSky/Assets/Script/EnnemyDetectZone.cs
+++ HollowSky/Assets/Script/EnnemyDetectZone.cs
@@ -8,6 +8,12 @@ public class EnnemyDetectZone : MonoBehaviour
 
     public GameObject corpsEnnemy;
 
+    public float speed = 5;
+
+    public Rigidbody rb;
+
+    Vector3 dir;
+
     void Start()
     {
         
@@ -16,14 +22,26 @@ public class EnnemyDetectZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = transform.forward * dir.z + transform.up * dir.y + transform.right * dir.x;
+        Vector3 velocity = move * speed;
+        Vector3 rbVelocity = new Vector3(velocity.x, velocity.y, velocity.z);
+        rb.velocity = rbVelocity;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.layer == 8)
+        if (other.gameObject.layer == 8)
         {
-            
+            dir =  other.gameObject.transform.position - corpsEnnemy.transform.position;
+            Debug.Log(dir);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            dir = Vector3.zero;
         }
     }
 
